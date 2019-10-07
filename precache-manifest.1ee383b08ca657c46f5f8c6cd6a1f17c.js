c51b230bcfbcd494330c96ac5f0b0dd23b03bf3f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-07 06:38:18 +0000

    Deploy LinYenCheng/heros to github.com/LinYenCheng/heros.git:gh-pages

---
+++ precache-manifest.1ee383b08ca657c46f5f8c6cd6a1f17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e79dd4877af028319b51e9f573059e",
+    "url": "/heros/index.html"
+  },
+  {
+    "revision": "f48e8a6fa344b4433e87",
+    "url": "/heros/static/css/main.606e2bf7.chunk.css"
+  },
+  {
+    "revision": "54136f51a9cc863e67f4",
+    "url": "/heros/static/js/2.7edf8d20.chunk.js"
+  },
+  {
+    "revision": "f48e8a6fa344b4433e87",
+    "url": "/heros/static/js/main.c1ddcbc2.chunk.js"
+  },
+  {
+    "revision": "614b329907b1bd82bd8c",
+    "url": "/heros/static/js/runtime~main.0080e84b.js"
+  }
+]);
